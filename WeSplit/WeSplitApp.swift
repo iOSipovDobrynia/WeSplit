fd4e45bf958efd6ddcfccd22b287dd37ab6dce3b
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-10-05 14:30:15 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Goodwasp on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
